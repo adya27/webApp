78e4351092c9fb514fc072e7d5ed7ec17e0ea81a
adya27 <31897074+adya27@users.noreply.github.com>
2020-10-13 00:03:13 +0300

    web app

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.listen(4444, () => {console.log("Appliction server is running on port 4444")});
+app.get("/", () => console.log("This is callback for app.get ('/')"));
+app.get("/about", () => console.log("This is callback for app.get ('/about')"));
+
+app.listen(4444, () => {
+  console.log("Appliction server is running on port 4444");
+});
