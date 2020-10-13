7af7929afd9bf66d357cccefebc6fe212f1d09fd
adya27 <31897074+adya27@users.noreply.github.com>
2020-10-13 22:44:42 +0300

    "add"

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const exhbrs = require("express-handlebars");
+const products = require("./products.json");
+const port = 4444;
 
 const app = express();
 
-app.get("/", () => console.log("This is callback for app.get ('/')"));
-app.get("/about", () => console.log("This is callback for app.get ('/about')"));
+app.use(express.static("public"));
+app.set("view engine", "hbs");
+app.engine("hbs", exhbrs({ extname: "hbs" }));
 
-app.listen(4444, () => {
-  console.log("Appliction server is running on port 4444");
+app.get("/", (req, res) => {
+  res.render("home", { title: "Main" });
+});
+app.get("/about", (req, res) => {
+  res.render("about", { cssFileName: "about", title: "О нас" });
+});
+app.get("/products", (req, res) => {
+  res.render("products", {
+    products: products,
+    cssFileName: "products",
+    title: "Продукты",
+  });
+});
+
+app.get("/product/:productId", (req, res) => {
+  console.log(req.params);
+  const product = products.find((p) => p.id === req.params.productId);
+  res.render("product", { product, cssFileName: "products" });
+});
+
+app.listen(port, () => {
+  console.log(`Appliction server is running on port ${port}`);
 });
